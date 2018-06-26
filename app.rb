83ee4615458ef941810de3ee64ed1eb5ec32244f
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-26 14:21:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Seema Shaik!"
+  end
+
+  get '/hometown' do
+    "My hometown is Ellicott City"
+  end
+
+  get '/favorite-song' do
+    "My favorite song os Airplane pt.2"
+  end
+
 end
