002163f255a2318232510945b61cd830492ad582
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-26 14:23:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Seema Shaik!"
+  end
+
+  get '/hometown' do
+    "My hometown is Ellicott City"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Airplane pt.2"
+  end
+
 end
